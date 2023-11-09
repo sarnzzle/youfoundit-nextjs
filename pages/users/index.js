8d8748182466461e43cb03d45b7c2bb5d7ac95ab
Matt Sarnak <sarnak@gmail.com>
2023-11-09 07:42:03 -0600

    slug

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,10 +1,14 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
 import JokeBlock from "@components/JokeBlock";
 
 export default function Users() {
+  const router = useRouter();
+  const slug = router.query.slug;
+
   return (
     <div className="container">
       <Head>
@@ -16,7 +20,7 @@ export default function Users() {
         <Header title="Users" />
         <hr />
         <p className="description">
-          All users.
+          {slug || "All users."}
         </p>
         <FeedbackForm />
         <JokeBlock />
